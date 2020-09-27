9c9d4b57b3f3eab920b9cda10b78e40f2e01317f
Ivan Penales <ipenales.dev@gmail.com>
2020-09-26 21:44:11 -0600

    fix add button class

--- ipenales-GearBook/app/src/main/java/com/example/gearbook/AddBtn.java
+++ ipenales-GearBook/app/src/main/java/com/example/gearbook/AddBtn.java
@@ -1,30 +1,35 @@
 package com.example.gearbook;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.fragment.app.FragmentManager;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class AddBtn {
     View rootView;
     Context curContext;
-    final EditText date = rootView.findViewById(R.id.date_editText);
-    final EditText maker = rootView.findViewById(R.id.maker_editText);
-    final EditText desc = rootView.findViewById(R.id.desc_editText);
-    final EditText price = rootView.findViewById(R.id.price_editText);
-    final EditText comment = rootView.findViewById(R.id.comment_editText);
-    final FloatingActionButton addGearButton = rootView.findViewById(curContext.id.add_gear_button_button);
-    public AddBtn(View argView,Context argContext){
-        rootView = argView;
-        curContext = argContext;
+    FloatingActionButton addGearButton;
+    FragmentManager mainActivity;
+    ArrayList<EditText> attList;
+    public AddBtn(ArrayList<EditText> references,
+                  FloatingActionButton argBtn,androidx.fragment.app.FragmentManager argActivity) {
+        attList = references;
+        mainActivity = argActivity;
+        addGearButton = argBtn;
+        addGearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //this will be triggered when the add button is clicked
+                new AddGearFragment(attList.get(0), attList.get(1),
+                        attList.get(2), attList.get(3), attList.get(4))
+                        .show(mainActivity, "ADD_GEAR");
+            }
+        });
     }
-    addGearButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            new AddGearFragment(date,maker,desc,price,comment)
-                    .show(getSupportFragmentManager(), "ADD_GEAR");
-        }
-    });
-
 }
--- ipenales-GearBook/app/src/main/java/com/example/gearbook/MainActivity.java
+++ ipenales-GearBook/app/src/main/java/com/example/gearbook/MainActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.View;
 
@@ -14,18 +16,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements AddGearFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
-        final EditText date = findViewById(R.id.date_editText);
-        final EditText maker = findViewById(R.id.maker_editText);
-        final EditText desc = findViewById(R.id.desc_editText);
-        final EditText price = findViewById(R.id.price_editText);
-        final EditText comment = findViewById(R.id.comment_editText);
-        final FloatingActionButton addCityButton = findViewById(R.id.add_city_button);
+        new AddBtn(new ArrayList<EditText>(){
+            {
+                add((EditText)findViewById(R.id.date_editText));
+                add((EditText)findViewById(R.id.maker_editText));
+                add((EditText)findViewById(R.id.desc_editText));
+                add((EditText)findViewById(R.id.price_editText));
+                add((EditText)findViewById(R.id.comment_editText));
+            }
+        },(FloatingActionButton) findViewById(R.id.add_gear_button),getSupportFragmentManager());
         setContentView(R.layout.activity_main);
     }
     //need to call setAdapter on the listView so that each element has the view
